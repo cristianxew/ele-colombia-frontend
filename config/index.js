d8981eaccfc51cba5240a0eff4e56b13a358c33f
Cristian Bernal <cristianxsa15@gmail.com>
2021-04-27 22:09:22 +0200

    Added URLs

--- config/index.js
+++ config/index.js
@@ -1,1 +1,3 @@
-export const URL_API = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000"
+export const URL_API = process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337"
+
+export const NEXT_URL = process.env.NEXT_PUBLIC_FRONTEND_URL || "http://localhost:3000"
