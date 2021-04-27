2ae7fff94170d143baa11bdc53d54fcd61e68c42
Cristian Bernal <cristianxsa15@gmail.com>
2021-04-27 22:10:57 +0200

    Created login api route with next

---
+++ pages/api/login.js
@@ -0,0 +1,29 @@
+import { URL_API } from "../../config"
+
+export default async (req, res) => {
+  if (req.method === 'POST') {
+    const { identifier, password } = req.body
+    const strapiRes = await fetch(`${URL_API}/auth/local`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        identifier,
+        password
+      })
+    })
+    const data = await strapiRes.json()
+    console.log(data)
+
+    if (strapiRes.ok) {
+      // @todo set cookie
+      res.status(200).json({ user: data.user })
+    } else {
+      res.status(data.statusCode).json({ message: data.message[0].messages[0].message })
+    }
+  } else {
+    res.setHeader("Allow", ["POST"])
+    res.status(405).json({ message: `Method ${rep.method} not allowed` })
+  }
+}
